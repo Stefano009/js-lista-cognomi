b25cc409ff3f11175e216b1a71ba8dec583d1c8d
Stefano <stefano.funaro@gmail.com>
2021-07-20 15:18:06 +0200

    Esercizio-completato

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+3. stampa la lista ordinata alfabeticamente
+4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+*/
+
+
+//ask user for his surname
+var userSurname = prompt("per favore inserisca il suo cognome");
+var surnamesList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var sortedSurnameList = [];
+// forced the input with capital first letter to maintain the code working
+var userSurnameCapitalLetter;
+userSurnameCapitalLetter = userSurname.charAt(0).toUpperCase() + userSurname.slice(1);
+//push it in an array
+surnamesList.push(userSurnameCapitalLetter);
+console.log(surnamesList);
+// print surname list alphabetically
+sortedSurnameList.push(...surnamesList);
+sortedSurnameList.sort();
+console.log(sortedSurnameList);
+// print "human" position of the list where user is put
+console.log(sortedSurnameList.indexOf(userSurnameCapitalLetter) + 1);
